a744efeb526cbc08ee890fc4bf7cdabf9d1dec43
asistradition <asistradition@users.noreply.github.com>
2019-11-15 17:05:06 -0500

    No dashes

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="ASsc-RNA-seq",
-    packages=find_packages(include=["ASsc-RNA-seq", "ASsc-RNA-seq.*"], exclude=["tests", "*.tests"]),
+    name="ASscRNAseq",
+    packages=find_packages(include=["ASscRNAseq", "ASscRNAseq.*"], exclude=["tests", "*.tests"]),
     zip_safe=False,
     tests_require=["coverage", "nose"],
     test_suite="nose.collector",
